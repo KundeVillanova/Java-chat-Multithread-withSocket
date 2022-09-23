e6457a343e0da4ebf137a6e3f0cc2685a10f1765
João Victor <86169183+KundeVillanova@users.noreply.github.com>
2022-09-23 14:43:03 -0300

    Finished
    Final commit.

---
+++ JavaChat/src/main/java/redes/csi/javachat/JavaChatApplication.java
@@ -0,0 +1,10 @@
+package redes.csi.javachat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaChatApplication.class, args);
+    }
+}
